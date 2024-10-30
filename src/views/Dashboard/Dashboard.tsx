81198fb63e0872b8ce87a9e66e9ae32deae1030f
Piotr Pilz <nerdyone@ppilz.com>
2024-10-31 00:38:17 +0100

    refactor: remove unused code

--- src/views/Dashboard/Dashboard.tsx
+++ src/views/Dashboard/Dashboard.tsx
@@ -1,6 +1,5 @@
 import { getServers } from "@/api/servers"
 import { useQuery } from "@tanstack/react-query"
-import { useEffect } from "react"
 
 interface IServer {
   name: string
@@ -10,10 +9,6 @@ interface IServer {
 
 export const Dashboard:React.FC = () => {
   const serversQuery = useQuery({ queryKey: ['servers'], queryFn: getServers })
-
-  useEffect(() => {
-    console.log(serversQuery.data)
-  }, [serversQuery.data])
 
   return (
     <div>
