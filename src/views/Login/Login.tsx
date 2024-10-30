f12bebb72918ec55e88c5d161a0f3e616484acae
Piotr Pilz <nerdyone@ppilz.com>
2024-10-31 00:38:59 +0100

    Add `wrong credentials` login error

--- src/views/Login/Login.tsx
+++ src/views/Login/Login.tsx
@@ -1,22 +1,36 @@
 import { useUserContext } from "@/hooks/useUserContext"
+import ApiError from "@/utils/error"
 import { useState } from "react"
 
 export const Login:React.FC = () => {
   const [username, setUsername] = useState<string>('')
   const [password, setPassword] = useState<string>('')
+  const [loginError, setLoginError] = useState<string | null>(null)
   const { login, isLoading } = useUserContext()
 
-  const handleLogin = () => {
-    login({ username, password })
+  const isFormValid = username.trim() !== '' && password.trim() !== '';
+
+  const handleLogin = async () => {
+    try {
+      await login({ username, password })
+    } catch (err) {
+      if (err instanceof ApiError && err.statusCode === 401) {
+        setLoginError('Wrong username or password')
+      }
+    }
   }
   return (
     <div>
+      {loginError
+        ? <div data-testid="login-error">{loginError}</div>
+        : null
+      }
       <div>
         tesonet
         <input
           data-testid="username-field"
           type="text"
-          placeholder="username"
+          placeholder="Username"
           onChange={(e) => setUsername(e.currentTarget.value)}
         />
       </div>
@@ -25,13 +39,18 @@ export const Login:React.FC = () => {
         <input
           data-testid="password-field"
           type="password"
-          placeholder="password"
+          placeholder="Password"
           onChange={(e) => setPassword(e.currentTarget.value)}
         />
       </div>
       <div>
-        <button onClick={handleLogin}>{isLoading ? 'logging in...' : 'Login'}</button>
+        <button
           data-testid="login-button"
+          onClick={handleLogin}
+          disabled={!isFormValid}
+        >
+          {isLoading ? 'logging in...' : 'Login'}
+        </button>
       </div>
     </div>
   )
